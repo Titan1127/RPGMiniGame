7d4205624e27d843ed21683aca952c7806581f74
Titan1127 <55869746+Titan1127@users.noreply.github.com>
2024-01-11 20:15:45 +0800

    Version 1

---
+++ src/settings.b0f61.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Sence/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"d6ceb"}};
